2c6c06446b5445b9c204ca66c563208999e5783c
Lisa Backer <lisa.backer@acquia.com>
2014-09-11 14:49:46 -0400

    Fix form comments.

--- src/Entity/Form.php
+++ src/Entity/Form.php
@@ -4,10 +4,7 @@
  * @file
  * Contains Drupal\mollom\Entity\Form.
  *
- * This contains our entity class.
- *
- * Originally based on code from blog post at
- * http://previousnext.com.au/blog/understanding-drupal-8s-config-entities
+ * This contains a form that is protected by Mollom.
  */
 
 namespace Drupal\mollom\Entity;
@@ -15,32 +12,7 @@ namespace Drupal\mollom\Entity;
 use Drupal\Core\Config\Entity\ConfigEntityBase;
 
 /**
- * Defines the robot entity.
- *
- * The lines below, starting with '@ConfigEntityType,' are a plugin annotation.
- * These define the entity type to the entity type manager.
- *
- * The properties in the annotation are as follows:
- *  - id: The machine name of the entity type.
- *  - label: The human-readable label of the entity type. We pass this through
- *    the "@Translation" wrapper so that the multilingual system may
- *    translate it in the user interface.
- *  - controllers: An array specifying controller classes that handle various
- *    aspects of the entity type's functionality. Below, we've specified
- *    controllers which can list, add, edit, and delete our robot entity, and
- *    which control user access to these capabilities.
- *  - config_prefix: This tells the config system the prefix to use for
- *    filenames when storing entities. This means that the default entity we
- *    include in our module has the filename
- *    'config_entity_example.robot.marvin.yml'.
- *  - entity_keys: Specifies the class properties in which unique keys are
- *    stored for this entity type. Unique keys are properties which you know
- *    will be unique, and which the entity manager can use as unique in database
- *    queries.
- *
- * @see http://previousnext.com.au/blog/understanding-drupal-8s-config-entities
- * @see annotation
- * @see Drupal\Core\Annotation\Translation
+ * Defines the form entity.
  *
  * @ingroup mollom
  *
