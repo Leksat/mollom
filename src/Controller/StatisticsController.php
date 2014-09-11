bd6daba9e548322f8bb58daa018ac10411bce5d5
Thomas Meire <thomas.meire@acquia.com>
2014-09-11 15:31:07 -0400

    Re-add the statistics page

---
+++ src/Controller/StatisticsController.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mollom\Form\Statistics.
+ */
+
+namespace Drupal\mollom\Controller;
+
+use Drupal\Component\Utility\String;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Template\Attribute;
+
+/**
+ * Defines a page that shows statistics.
+ */
+class StatisticsController extends ControllerBase {
+  public function content() {
+
+    $config = $this->config('mollom.settings');
+
+    $embed_attributes = array(
+      'src' => 'https://mollom.com/statistics.swf?key=' . String::checkPlain($config->get('keys.public', '')),
+      'quality' => 'high',
+      'width' => '100%',
+      'height' => '430',
+      'name' => 'Mollom',
+      'align' => 'middle',
+      'play' => 'true',
+      'loop' => 'false',
+      'allowScriptAccess' => 'sameDomain',
+      'type' => 'application/x-shockwave-flash',
+      'pluginspage' => 'http://www.adobe.com/go/getflashplayer',
+      'wmode' => 'transparent'
+    );
+    return array(
+      '#type' => 'item',
+      '#markup' => '<embed' . new Attribute($embed_attributes) . '></embed>',
+    );
+  }
+}
